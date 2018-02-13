3fe6002d70c69910fc6273e4282b9355f2def256
Serhii <sergey.petrunya@gmail.com>
2018-02-13 13:25:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-var add = (a, b) => a + b; 
+var add = (a, b) => a + b;
+var subtract = (a, b) => a - b;
+var multiply = (a, b) => a * b;
+var divide = (a, b) => a / b;
+var inc = (a) => a + 1;
+var dec = (a) => a - 1;
+var makeInt = (a) => parseInt(a, 10);
+var makeInt = (a) => parseInt(a, 2);
+var makeInt = (a) => isNaN(a);
+var preserveDecimal = (a) => parseFloat(a);
